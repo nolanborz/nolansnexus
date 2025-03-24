7ea12c4d93a610ab437106e66ead2ef918a0782f
nolanborz <nolanrborzoni@gmail.com>
2025-03-24 20:18:42 +0900

    add word counter, format articles to half competence

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,16 @@
 module ArticlesHelper
+  def reading_time(content)
+    text = ActionView::Base.full_sanitizer.sanitize(content)
+
+    all_the_words = text.split.size
+
+    minutes = (all_the_words / 225.0).ceil
+
+    # makes sure we don't have like less than a minute that'd be awkward
+    ttr = [ 1, minutes ].max
+    if ttr == 1
+      return ttr.to_s + " minute"
+    end
+    ttr.to_s + " minutes"
+  end
 end
